03b63982f1ab56cd12568b54be85b0bae7f8f181
mathew <69637595+wa2f@users.noreply.github.com>
2020-11-25 16:00:35 +0300

    index and layout

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, redirect, jsonify, render_template, request, session
-
-app = Flask(__name__)
-
-# login box start here
-@app.route("/")
-def index():
-    return render_template("index.html")
-
